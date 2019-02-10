6eaba25fdcedb92e20b8666ee52c2d101a75dcab
Gilbert Torchon <gilbert@noukod.com>
2019-02-10 13:46:10 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,12 @@
+function produceDrivingRange(blockRange){
+  return function(startingBlock,endingBlock){
+      startingBlock=parseInt(startingBlock.replace(/[a-zA-Z]/g,""));
+      endingBlock=parseInt(endingBlock.replace(/[a-zA-Z]/g,""));
+      const distanceBetweenBlocks=Math.abs(startingBlock-endingBlock);
+      if (distanceBetweenBlocks>blockRange){
+        return `${distanceBetweenBlocks-blockRange} blocks out of range`;
+      }else{
+        return `within range by ${blockRange-distanceBetweenBlocks}`
+      }
+  }
+}
